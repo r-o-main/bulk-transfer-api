97977dbd359b691d87ceaea1f7bfe79c2aadce54
Romain <romain.bouteloup@alcmeon.com>
2025-06-28 16:24:55 +0200

    Handle amounts errors

--- app/routers/transfers.py
+++ app/routers/transfers.py
@@ -1,10 +1,16 @@
 import uuid
+import decimal
+import logging
 
 from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
 # from app.models.schemas import BulkTransferRequest
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 from uuid import UUID
+
+
+logger = logging.getLogger(__name__)
 
 
 router = APIRouter()  # https://fastapi.tiangolo.com/reference/apirouter
@@ -12,12 +18,12 @@ router = APIRouter()  # https://fastapi.tiangolo.com/reference/apirouter
 
 class CreditTransfer(BaseModel):  # todo enforce strict mode
     transfer_id: Optional[UUID] = None
-    amount: str
-    currency: str
-    counterparty_name: str
-    counterparty_bic: str
-    counterparty_iban: str
-    description: str
+    amount: str = Field(..., min_length=1)
+    currency: str = Field(..., min_length=3, max_length=3)
+    counterparty_name: str = Field(..., min_length=1)
+    counterparty_bic: str = Field(..., min_length=1)  # todo check BIC length
+    counterparty_iban: str = Field(..., min_length=1)  # todo check IBAN length
+    description: str = Field(..., min_length=10)
 
     model_config = {
         "extra": "forbid"
@@ -26,8 +32,8 @@ class CreditTransfer(BaseModel):  # todo enforce strict mode
 
 class BulkTransferRequest(BaseModel):  # todo enforce strict mode
     # bulk_id: UUID  # todo Milestone 3
-    organization_bic: str
-    organization_iban: str
+    organization_bic: str = Field(..., min_length=1)  # todo check BIC length
+    organization_iban: str = Field(..., min_length=1)  # todo check IBAN length
     credit_transfers: List[CreditTransfer]
 
     model_config = {
@@ -35,17 +41,102 @@ class BulkTransferRequest(BaseModel):  # todo enforce strict mode
     }
 
 
+class BulkTransferSuccessResponse(BaseModel):
+    bulk_id: str  #  UUID
+    message: str
+
+
+class ErrorDetails(BaseModel):
+    reason: str  # todo Enum
+    details: str
+
+
+class BulkTransferErrorResponse(BulkTransferSuccessResponse):
+    error: ErrorDetails
+
+
+def _bulk_error(bulk_id: UUID, reason: str, error_details: str) -> JSONResponse:
+    return JSONResponse(
+        status_code=422,
+        content=BulkTransferErrorResponse(
+            bulk_id=str(bulk_id), message="Bulk transfer denied", error=ErrorDetails(reason=reason, details=error_details)
+        ).model_dump()
+    )
+
+def reply_not_enough_funds_error(bulk_id: UUID, error_details: Optional[str] = None) -> JSONResponse:
+    return _bulk_error(
+        bulk_id=bulk_id,
+        reason='insufficient-account-balance',  # todo ENUM
+        error_details=error_details if error_details else "Not enough funds"
+    )
+
+def reply_amounts_should_be_positive_error(bulk_id: UUID, error_details: Optional[str] = None) -> JSONResponse:
+    return _bulk_error(
+        bulk_id=bulk_id,
+        reason='negative-or-null-amounts',  # todo ENUM
+        error_details=error_details if error_details else "All amounts should be strictly greater than zero"
+    )
+
+
+def reply_amounts_invalid_format_error(bulk_id: UUID, error_details: Optional[str] = None) -> JSONResponse:
+    return _bulk_error(
+        bulk_id=bulk_id,
+        reason='invalid-amount',  # todo ENUM
+        error_details=error_details if error_details else "All amounts should be numbers and not have more than 2 decimal places"
+    )
+
+
+def to_cents(amount_in_euros_str: str) -> int:
+    try:
+        amount_in_euros = decimal.Decimal(amount_in_euros_str)
+    except Exception:
+        raise ValueError(f"Invalid amount: {amount_in_euros_str}")
+    rounded_amount_in_euros = amount_in_euros.quantize(decimal.Decimal("0.01"), rounding=decimal.ROUND_HALF_UP)
+    if amount_in_euros != rounded_amount_in_euros:
+        raise ValueError(f"More than 2 decimal places is not allowed: {amount_in_euros_str}")
+    amount_in_cents = rounded_amount_in_euros * 100
+    return int(amount_in_cents)
+
+
 @router.post(
     "/bulk",
     status_code=status.HTTP_201_CREATED,
-    responses={
-        400: {"description": "Bad Request"},
-        404: {"description": "Organization not found"},
-        422: {"description": "Insufficient funds"},
+    response_model=BulkTransferSuccessResponse,
+    responses={  # https://fastapi.tiangolo.com/advanced/additional-responses/
+        400: {"model": BulkTransferErrorResponse, "description": "Bad Request"},
+        404: {"model": BulkTransferErrorResponse, "description": "Organization or Account not found"},
+        # 422: {"description": "Payload does not match spec"},
+        422: {"model": BulkTransferErrorResponse, "description": "Bulk transfer denied"},  # todo list of reasons (provide details in response) + handle mismatch patload vs Pydantic model
     }
 )
 def create_bulk_transfer(request: BulkTransferRequest):  # todo check async
+    """
+    /docs
+    """
     # todo
+    bulk_id = uuid.uuid4()  # todo handle idempotency
     # checks: first additional validation (such as amounts are positive and can be converted to int), amounts are enough, etc.
+    # decimal_context = decimal.getcontext()
+    # decimal_context.prec = 2
+    # decimal_context.rounding = decimal.ROUND_HALF_UP
+
+    amounts_in_cents = []
+    try:
+        amounts_in_cents = [to_cents(amount_in_euros_str=credit_transfer.amount) for credit_transfer in
+                            request.credit_transfers]
+    except ValueError as e:
+        logger.error(f"bulk_id={bulk_id} could not process request: {e}")  # todo add logging context
+        return reply_amounts_invalid_format_error(bulk_id=bulk_id, error_details=str(e))
+
+    all_transfer_amounts_are_valid = all(amount > 0 for amount in amounts_in_cents)
+    if not all_transfer_amounts_are_valid:
+        logger.error(f"bulk_id={bulk_id} could not process request as not all amounts are > 0: {amounts_in_cents}")  # todo add logging context
+        return reply_amounts_should_be_positive_error(bulk_id=bulk_id)
+        # raise HTTPException(status_code=422, detail="Bulk transfer denied")
+
     # return {"message": "Bulk transfer accepted", "bulk_id": str(request.bulk_id)}
-    return {"message": "Bulk transfer accepted", "bulk_id": str(uuid.uuid4())}
+    return {"message": "Bulk transfer accepted", "bulk_id": str(bulk_id)}
+    # return JSONResponse(
+    #     status_code=201,
+    #     content=BulkTransferSuccessResponse(bulk_id=str(bulk_id), message="Bulk transfer accepted").model_dump()
+    # )
--- tests/test_bulk_transfers.py
+++ tests/test_bulk_transfers.py
@@ -1,10 +1,15 @@
 import json
+import logging
 import os
 
 import pytest
 
 from fastapi.testclient import TestClient
 from app.main import app
+
+
+logger = logging.getLogger()
+
 
 client = TestClient(app)  # https://fastapi.tiangolo.com/reference/testclient/, https://fastapi.tiangolo.com/tutorial/testing/
 
@@ -27,12 +32,12 @@ def test_transfers_bulk__when_valid_payload__should_return_201(sample_file):
 
 @pytest.mark.parametrize("assert_message, payload", [
     ('when missing mandatory request key (credit_transfers)', {
-        "organization_bic": "VALIDBIC",
-        "organization_iban": "VALIDIBAN",
+        "organization_bic": "VALID_BIC",
+        "organization_iban": "VALID_IBAN",
     }),
     ('when missing mandatory credit transfer key (amount)', {
-        "organization_bic": "VALIDBIC",
-        "organization_iban": "VALIDIBAN",
+        "organization_bic": "VALID_BIC",
+        "organization_iban": "VALID_IBAN",
         "credit_transfers": [
             {
                 "currency": "EUR",
@@ -44,8 +49,8 @@ def test_transfers_bulk__when_valid_payload__should_return_201(sample_file):
         ]
     }),
     ('when amount is an int', {
-        "organization_bic": "VALIDBIC",
-        "organization_iban": "VALIDIBAN",
+        "organization_bic": "VALID_BIC",
+        "organization_iban": "VALID_IBAN",
         "credit_transfers": [
             {
                 "amount": 15,
@@ -58,14 +63,14 @@ def test_transfers_bulk__when_valid_payload__should_return_201(sample_file):
         ]
     }),
     ('when additional request key', {
-        "organization_bic": "VALIDBIC",
-        "organization_iban": "VALIDIBAN",
+        "organization_bic": "VALID_BIC",
+        "organization_iban": "VALID_IBAN",
         "credit_transfers": [],
         "additional_key": "whatever"
     }),
     ('when additional credit_transfers key', {
-        "organization_bic": "VALIDBIC",
-        "organization_iban": "VALIDIBAN",
+        "organization_bic": "VALID_BIC",
+        "organization_iban": "VALID_IBAN",
         "credit_transfers": [
             {
                 "amount": "15.2",
@@ -82,3 +87,193 @@ def test_transfers_bulk__when_valid_payload__should_return_201(sample_file):
 def test_transfers_bulk__when_invalid_payload_model__should_return_422(assert_message, payload):
     response = client.post(url="/transfers/bulk", json=payload)
     assert response.status_code == 422, f"{assert_message}: {response.json()}"
+
+
+@pytest.mark.parametrize("assert_message, payload", [
+    # ('when organization is not known', {
+    #     "organization_bic": "UNKNOWN_BIC",
+    #     "organization_iban": "VALID_IBAN",
+    #     "credit_transfers": [],
+    # }),
+    # ('when customer bank account is not known', {
+    #     "organization_bic": "VALID_BIC",
+    #     "organization_iban": "UNKNOWN_IBAN",
+    #     "credit_transfers": [],
+    # }),
+    # ('when customer bank and account are not known', {
+    #     "organization_bic": "UNKNOWN_BIC",
+    #     "organization_iban": "UNKNOWN_IBAN",
+    #     "credit_transfers": [],
+    # }),
+    # ('when organization BIC is not valid', {
+    #     "organization_bic": "INVALID_BIC",
+    #     "organization_iban": "VALID_IBAN",
+    #     "credit_transfers": [],
+    # }),
+    # ('when organization IBAN is not valid', {
+    #     "organization_bic": "VALID_BIC",
+    #     "organization_iban": "INVALID_IBAN",
+    #     "credit_transfers": [],
+    # }),
+    # ('when organization IBAN and BIC are not valid', {
+    #     "organization_bic": "INVALID_BIC",
+    #     "organization_iban": "INVALID_IBAN",
+    #     "credit_transfers": [],
+    # }),
+    ('when amount to transfer < 0', {
+        "organization_bic": "INVALID_BIC",
+        "organization_iban": "INVALID_IBAN",
+        "credit_transfers": [
+            {
+                "amount": "61238",
+                "currency": "EUR",
+                "counterparty_name": "Wile E Coyote",
+                "counterparty_bic": "ZDRPLBQI",
+                "counterparty_iban": "DE9935420810036209081725212",
+                "description": "//TeslaMotors/Invoice/12"
+            },
+            {
+                "amount": "-15",
+                "currency": "EUR",
+                "counterparty_name": "Bip Bip",
+                "counterparty_bic": "CRLYFRPPTOU",
+                "counterparty_iban": "EE383680981021245685",
+                "description": "Wonderland/4410",
+            }
+        ],
+    }),
+    ('when amount to transfer == 0', {
+        "organization_bic": "INVALID_BIC",
+        "organization_iban": "INVALID_IBAN",
+        "credit_transfers": [
+            {
+                "amount": "61238",
+                "currency": "EUR",
+                "counterparty_name": "Wile E Coyote",
+                "counterparty_bic": "ZDRPLBQI",
+                "counterparty_iban": "DE9935420810036209081725212",
+                "description": "//TeslaMotors/Invoice/12"
+            },
+            {
+                "amount": "0",
+                "currency": "EUR",
+                "counterparty_name": "Bip Bip",
+                "counterparty_bic": "CRLYFRPPTOU",
+                "counterparty_iban": "EE383680981021245685",
+                "description": "Wonderland/4410",
+            }
+        ],
+    }),
+    ('when amount to transfer is null', {
+        "organization_bic": "INVALID_BIC",
+        "organization_iban": "INVALID_IBAN",
+        "credit_transfers": [
+            {
+                "amount": None,
+                "currency": "EUR",
+                "counterparty_name": "Wile E Coyote",
+                "counterparty_bic": "ZDRPLBQI",
+                "counterparty_iban": "DE9935420810036209081725212",
+                "description": "//TeslaMotors/Invoice/12"
+            },
+        ],
+    }),
+    ('when amount to transfer is empty', {
+        "organization_bic": "INVALID_BIC",
+        "organization_iban": "INVALID_IBAN",
+        "credit_transfers": [
+            {
+                "amount": "",
+                "currency": "EUR",
+                "counterparty_name": "Wile E Coyote",
+                "counterparty_bic": "ZDRPLBQI",
+                "counterparty_iban": "DE9935420810036209081725212",
+                "description": "//TeslaMotors/Invoice/12"
+            },
+        ],
+    }),
+    ('when amount to transfer has more than 2 decimal places', {
+        "organization_bic": "VALID_BIC",
+        "organization_iban": "VALID_IBAN",
+        "credit_transfers": [
+            {
+                "amount": "13.2356",
+                "currency": "EUR",
+                "counterparty_name": "Wile E Coyote",
+                "counterparty_bic": "ZDRPLBQI",
+                "counterparty_iban": "DE9935420810036209081725212",
+                "description": "//TeslaMotors/Invoice/12"
+            },
+        ],
+    }),
+    ('when amount to transfer is invalid', {
+        "organization_bic": "VALID_BIC",
+        "organization_iban": "VALID_IBAN",
+        "credit_transfers": [
+            {
+                "amount": "aaaaa",
+                "currency": "EUR",
+                "counterparty_name": "Wile E Coyote",
+                "counterparty_bic": "ZDRPLBQI",
+                "counterparty_iban": "DE9935420810036209081725212",
+                "description": "//TeslaMotors/Invoice/12"
+            },
+        ],
+    }),
+    ('when description is too short', {
+        "organization_bic": "VALID_BIC",
+        "organization_iban": "VALID_IBAN",
+        "credit_transfers": [
+            {
+                "amount": "15",
+                "currency": "EUR",
+                "counterparty_name": "Wile E Coyote",
+                "counterparty_bic": "ZDRPLBQI",
+                "counterparty_iban": "DE9935420810036209081725212",
+                "description": "toto"
+            },
+        ],
+    }),
+    # ('when total amount to transfer is higher than actual account balance', {
+    #     "organization_bic": "INVALID_BIC",
+    #     "organization_iban": "INVALID_IBAN",
+    #     "credit_transfers": [
+    #         {
+    #             "amount": "61238",
+    #             "currency": "EUR",
+    #             "counterparty_name": "Wile E Coyote",
+    #             "counterparty_bic": "ZDRPLBQI",
+    #             "counterparty_iban": "DE9935420810036209081725212",
+    #             "description": "//TeslaMotors/Invoice/12"
+    #         },
+    #     ],
+    # }),
+    # ('when total amount to transfer is higher than actual account balance', {
+    #     "organization_bic": "INVALID_BIC",
+    #     "organization_iban": "INVALID_IBAN",
+    #     "credit_transfers": [
+    #         {
+    #             "amount": "61238",
+    #             "currency": "EUR",
+    #             "counterparty_name": "Wile E Coyote",
+    #             "counterparty_bic": "ZDRPLBQI",
+    #             "counterparty_iban": "DE9935420810036209081725212",
+    #             "description": "//TeslaMotors/Invoice/12"
+    #         },
+    #         {
+    #             "amount": "999",
+    #             "currency": "EUR",
+    #             "counterparty_name": "Bugs Bunny",
+    #             "counterparty_bic": "RNJZNTMC",
+    #             "counterparty_iban": "FR0010009380540930414023042",
+    #             "description": "2020 09 24/2020 09 25/GoldenCarrot/"
+    #         }
+    #     ]
+    # }),
+])
+def test_transfers_bulk__should_return_422(assert_message, payload):
+    response = client.post(url="/transfers/bulk", json=payload)
+    print(f"++++ response={response.json()}")
+    assert response.status_code == 422, f"{assert_message}: {response.json()}"
+
+# todo test rounding strategy: "10.05"
